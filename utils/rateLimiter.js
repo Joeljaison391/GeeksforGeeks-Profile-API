1c3ecb801d902a29c12233174fc4813b570dacd9
Joel Jaison <joel221911@sahrdaya.ac.in>
2024-05-26 14:54:49 +0530

    chore: Update Dsa card and rate-limit

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
+  windowMs: 60 * 60 * 1000, 
   max: 100, 
   message: 'Too many requests from this IP, please try again after 15 minutes',
 });
